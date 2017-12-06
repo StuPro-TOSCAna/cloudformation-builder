2f8d95e81798f2531f3603f3287dee9854cb79af
mklopp <mklopp@outlook.de>
2017-12-06 17:59:47 +0100

    Add CFNFile, CFNCommand and empty classes for CFNService and CFNPackage also add them to Confgi

---
+++ src/main/java/com/scaleset/cfbuilder/ec2/metadata/CFNPackage.java
@@ -0,0 +1,9 @@
+package com.scaleset.cfbuilder.ec2.metadata;
+
+public class CFNPackage {
+    public String id;
+
+    public String getId(){
+        return this.id;
+    }
+}
---
+++ src/main/java/com/scaleset/cfbuilder/ec2/metadata/CFNService.java
@@ -0,0 +1,12 @@
+package com.scaleset.cfbuilder.ec2.metadata;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class CFNService {
+    public String id;
+
+    public String getId(){
+        return this.id;
+    }
+
+}

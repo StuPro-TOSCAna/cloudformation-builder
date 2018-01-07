5b856331b4d8718e83bc715be4a827adbc1db61c
Marvin Munoz Baron <mmunozba@outlook.com>
2018-01-07 18:51:31 +0100

    Add source property to CFNFile (#24)
    * Add source property and update comments
    
    * Fix NullPointerException on CFNFile
    
    * Make content and source of a CFN file mutually exclusive + tests
    
    * Test coverage

--- src/test/java/com/scaleset/cfbuilder/MetadataTest.java
+++ src/test/java/com/scaleset/cfbuilder/MetadataTest.java
@@ -13,11 +13,15 @@ import com.scaleset.cfbuilder.ec2.metadata.CFNPackage;
 import com.scaleset.cfbuilder.ec2.metadata.CFNService;
 import com.scaleset.cfbuilder.ec2.metadata.Config;
 import com.scaleset.cfbuilder.ec2.metadata.SimpleService;
+import org.junit.Assert;
 import org.junit.Test;
 
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.not;
 import static org.junit.Assert.assertNotNull;
 
 public class MetadataTest {
+
     @Test
     public void metadataTest() throws Exception {
         Template lampTemplate = new Template();
@@ -27,8 +31,23 @@ public class MetadataTest {
         System.err.println(lampTemplate.toString(true));
     }
 
+    @Test
+    public void fileTest() throws Exception {
+        //ensure that a CFNFile can only take Source or File
+        Template fileTemplate = new Template();
+        new FileModule().id("").template(fileTemplate).build();
+
+        assertNotNull(fileTemplate);
+        String templateString = fileTemplate.toString(true);
+        Assert.assertThat(templateString, containsString("source:"));
+        Assert.assertThat(templateString, not(containsString("content:")));
+
+        System.err.println(templateString);
+    }
+
     class MetadataModule extends Module {
-        private static final String KEYNAME_DESCRIPTION = "Name of an existing EC2 KeyPair to enable SSH access to the instances";
+        private static final String KEYNAME_DESCRIPTION = "Name of an existing EC2 KeyPair to enable SSH access to " +
+                "the instances";
         private static final String KEYNAME_TYPE = "AWS::EC2::KeyPair::KeyName";
         private static final String KEYNAME_CONSTRAINT_DESCRIPTION = "must be the name of an existing EC2 KeyPair.";
 
@@ -39,7 +58,8 @@ public class MetadataTest {
         public void build() throws Exception {
 
             Object keyName = option("KeyName").orElseGet(
-                    () -> strParam("KeyName").type(KEYNAME_TYPE).description(KEYNAME_DESCRIPTION).constraintDescription(KEYNAME_CONSTRAINT_DESCRIPTION));
+                    () -> strParam("KeyName").type(KEYNAME_TYPE).description(KEYNAME_DESCRIPTION)
+                            .constraintDescription(KEYNAME_CONSTRAINT_DESCRIPTION));
 
             Object cidrIp = "0.0.0.0/0";
             Object keyNameVar = template.ref("KeyName");
@@ -51,7 +71,14 @@ public class MetadataTest {
 
             CFNFile indexFile = new CFNFile("/var/www/html/index.php")
                     .setContent("<php?\nphpinfo()\n?>")
-                    .setMode("000600")
+                    .setEncoding("base64")
+                    .setMode("000644")
+                    .setOwner("www-data")
+                    .setGroup("www-data");
+
+            CFNFile indexFileWithUrl = new CFNFile("/var/www/html/index2.php")
+                    .setSource("https://s3-us-west-2.amazonaws.com/link/to/file")
+                    .setMode("000644")
                     .setOwner("www-data")
                     .setGroup("www-data");
 
@@ -62,6 +89,7 @@ public class MetadataTest {
 
             Config install = new Config(CFNINIT_CONFIG_INSTALL)
                     .putFile(indexFile)
+                    .putFile(indexFileWithUrl)
                     .putPackage(cfnPackage)
                     .putService(service);
 
@@ -72,7 +100,6 @@ public class MetadataTest {
 
             cfnInit.getOrAddConfig(CFNINIT_CONFIGSET, CFNINIT_CONFIG_CONFIGURE)
                     .putCommand(configure_mysql);
-
 
             SecurityGroup webServerSecurityGroup = resource(SecurityGroup.class, "WebServerSecurityGroup")
                     .groupDescription("Enable ports 80 and 22")
@@ -93,4 +120,49 @@ public class MetadataTest {
             output("websiteURL", publicDNSName, "URL for newly created LAMP stack");
         }
     }
+
+    class FileModule extends Module {
+        private static final String KEYNAME_DESCRIPTION = "Name of an existing EC2 KeyPair to enable SSH access to " +
+                "the instances";
+        private static final String KEYNAME_TYPE = "AWS::EC2::KeyPair::KeyName";
+        private static final String KEYNAME_CONSTRAINT_DESCRIPTION = "must be the name of an existing EC2 KeyPair.";
+
+        private static final String CFNINIT_CONFIGSET = "InstallAndRun";
+        private static final String CFNINIT_CONFIG_INSTALL = "Install";
+
+        public void build() throws Exception {
+
+            Object keyName = option("KeyName").orElseGet(
+                    () -> strParam("KeyName").type(KEYNAME_TYPE).description(KEYNAME_DESCRIPTION)
+                            .constraintDescription(KEYNAME_CONSTRAINT_DESCRIPTION));
+
+            Object cidrIp = "0.0.0.0/0";
+            Object keyNameVar = template.ref("KeyName");
+
+            CFNFile indexFile = new CFNFile("/var/www/html/index.php")
+                    .setSource("https://s3-us-west-2.amazonaws.com/link/to/file")
+                    .setContent("<php?\nphpinfo()\n?>")
+                    .setMode("000644")
+                    .setOwner("www-data")
+                    .setGroup("www-data");
+
+            Config install = new Config(CFNINIT_CONFIG_INSTALL)
+                    .putFile(indexFile);
+
+            CFNInit cfnInit = new CFNInit(CFNINIT_CONFIGSET)
+                    .addConfig(CFNINIT_CONFIGSET, install);
+
+            SecurityGroup webServerSecurityGroup = resource(SecurityGroup.class, "WebServerSecurityGroup")
+                    .groupDescription("Enable ports 80 and 22")
+                    .ingress(ingress -> ingress.cidrIp(cidrIp), "tcp", 80, 22);
+
+            Instance webServerInstance = resource(Instance.class, "WebServerInstance")
+                    .addCFNInit(cfnInit)
+                    .imageId("ami-0def3275")
+                    .instanceType("t2.micro")
+                    .securityGroupIds(webServerSecurityGroup)
+                    .keyName(keyNameVar);
+        }
+    }
 }
+

54a8c607d9074f7b257ffc4eb74181242d83f761
Marvin Munoz Baron <mmunozba@outlook.com>
2018-02-01 16:15:24 +0100

    Update ec2 and instanceProfile to allow s3 access

--- src/main/java/com/scaleset/cfbuilder/iam/InstanceProfile.java
+++ src/main/java/com/scaleset/cfbuilder/iam/InstanceProfile.java
@@ -1,11 +1,14 @@
 package com.scaleset.cfbuilder.iam;
 
 import com.scaleset.cfbuilder.annotations.Type;
+import com.scaleset.cfbuilder.core.Resource;
 
 @Type("AWS::IAM::InstanceProfile")
-public interface InstanceProfile {
+public interface InstanceProfile extends Resource{
 
     InstanceProfile path(Object value);
 
     InstanceProfile roles(Object... values);
+
+    InstanceProfile instanceProfileName(Object value);
 }

474c83dffebeade25529a61f9c93a28e7d830196
Marvin Munoz Baron <mmunozba@outlook.com>
2018-02-04 03:43:02 +0100

    Add AWS::EC2::Volume type

---
+++ src/main/java/com/scaleset/cfbuilder/ec2/Volume.java
@@ -0,0 +1,34 @@
+package com.scaleset.cfbuilder.ec2;
+
+import com.scaleset.cfbuilder.annotations.Type;
+import com.scaleset.cfbuilder.core.Taggable;
+
+/**
+ Constructs a {@code Volume} to create a new Amazon Elastic Block Store (Amazon EBS) volume.
+ <br>
+ Type: {@code AWS::EC2::Volume}
+
+ @see <a href="https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-properties-ec2-ebs-volume.html">Documentation Reference</a> */
+@Type("AWS::EC2::Volume")
+public interface Volume extends Taggable {
+    Volume autoEnableIO(boolean value);
+
+    Volume availabilityZone(Object value);
+
+    Volume encrypted(boolean value);
+
+    Volume iops(int value);
+
+    Volume kmsKeyId(Object value);
+
+    Volume size(int value);
+
+    Volume snapshotId(Object value);
+
+    Volume volumeType(Object value);
+
+    default Volume name(String name) {
+        tag("Name", name);
+        return this;
+    }
+}
